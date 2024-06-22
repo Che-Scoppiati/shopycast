65c05b17e8880d505dc5c3d050481878a8db77ab
caso <matteocasonato@protonmail.com>
2024-06-22 11:43:20 +0200

    add basic layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,8 @@
 import { shopifyClient } from "@/lib/shopify";
 import { FRAMES_BASE_PATH, appURL } from "@/lib/utils";
-import { Button } from "@nextui-org/react";
+import { Button, Card, CardBody, CardHeader, Input, Image } from "@nextui-org/react";
 import { fetchMetadata } from "frames.js/next";
 import { Metadata } from "next";
-import Image from "next/image";
 
 export async function generateMetadata(): Promise<Metadata> {
   const epochTimestamp = new Date().getTime();
@@ -24,33 +23,64 @@ export default async function Home() {
   );
   // console.log("shopify", { data, errors, extensions }, errors?.graphQLErrors);
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      {data &&
-        data.products.nodes.map((product: any) => (
-          <div key={product.id} className="flex flex-col items-center">
-            <h2 className="text-2xl font-bold">{product.title}</h2>
-            <p className="text-sm">{product.description}</p>
-            <div className="flex flex-col items-center">
-              {product.variants.edges.map((edge: any) => (
-                <div key={edge.node.id}>
-                  <Image
-                    src={edge.node.image.url}
-                    alt={product.title}
-                    width={200}
-                    height={200}
-                  />
-                  <p>
-                    {edge.node.price.amount} {edge.node.price.currencyCode}
-                  </p>
-                  <p>{edge.node.availableForSale ? "Available" : "Sold out"}</p>
-                  <Button color="primary">
-                    Button
-                  </Button>
-                </div>
-              ))}
+    <main className="flex min-h-screen flex-col items-center justify-between p-20">
+      <div className="w-full flex flex-col gap-16">
+        <h1 className="text-6xl font-bold w-fit m-auto">Onchain Shop</h1>
+        <div className="w-full flex flex-col gap-16">
+          <div className="w-full flex justify-between">
+            <Input type="text" label="Your Shopify Key" className="w-[33%]" />
+            <p className="text-xl w-[33%] text-end">
+              Select your products and create a frame to embed on Farcaster!
+            </p>
+          </div>
+          <div className="w-full flex flex-col gap-6">
+            <div className="flex w-full justify-between">
+              <h2 className="text-3xl font-bold w-fit">Select Products</h2>
+              <Button size="md" color="success">
+                Create Frame
+              </Button>  
+            </div>
+            <div className="grid grid-cols-4 gap-6">
+              {data &&
+                data.products.nodes.map((product: any) => (
+                  <Card className="p-3 gap-4" key={product.id}>
+                    <CardHeader className="p-0 flex-col items-start gap-2">
+                      <h4 className="font-bold text-large leading-none">{product.title}</h4>
+                      <small className="text-default-500 leading-none">{product.description}</small>
+                    </CardHeader>
+                    <CardBody className="overflow-visible p-0">
+                      {product.variants.edges.map((edge: any) => (
+                        <div className="flex flex-col items-center gap-4" key={edge.node.id}>
+                          <Image
+                            src={edge.node.image.url}
+                            alt={product.title}
+                            className="object-cover rounded-xl aspect-square"
+                            width={300}
+                          />
+                          <div className="flex w-full justify-between">
+                            <div className="flex flex-col gap-1">
+                              <p className="leading-none">
+                                {edge.node.price.amount} {edge.node.price.currencyCode}
+                              </p>
+                              <p className="leading-none">
+                                {edge.node.availableForSale ? "Available" : "Sold out"}
+                              </p>
+                            </div>
+                            <Button color="success">
+                              Buy
+                            </Button>
+                          </div>
+                        </div>
+                      ))}
+                    </CardBody>
+                  </Card>
+                ))
+              }
             </div>
           </div>
-        ))}
+        </div>
+      </div>
+      
     </main>
   );
 }
