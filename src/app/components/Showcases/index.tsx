652adea2e5c25b9307ba1c64e195b4f9f4c396ab
caso <matteocasonato@protonmail.com>
2024-06-26 23:45:15 +0200

    start home, improve dashboard if empty

--- src/app/components/Showcases/index.tsx
+++ src/app/components/Showcases/index.tsx
@@ -5,7 +5,6 @@ import { useQuery } from "@tanstack/react-query";
 import { useEffect, useState } from "react";
 import { Showcase } from "@/lib/mongodb";
 import { ShowcaseCard } from "./ShowcaseCard";
-import { NavbarLink } from "../Navbar";
 import { CreateShowcaseModal } from "../CreateShowcaseModal";
 
 export const Showcases: React.FC = () => {
@@ -34,23 +33,21 @@ export const Showcases: React.FC = () => {
       <div className="flex w-full justify-between items-end">
         <div className="flex flex-col">
           <h1 className="text-3xl font-bold">Your Showcases</h1>
-          <h2 className="text-lg text-default-500">
-            Click on a Showcase to view, edit or delete it
-          </h2>
+          {showcases?.length && showcases?.length > 0 ? (
+            <h2 className="text-lg text-default-500">
+              Click on a Showcase to view, edit or delete it
+            </h2>
+          ) : (
+            <div className="flex gap-[6px]">
+              <h2 className="text-lg text-default-500">
+                You don&apos;t have any showcases yet. To get started, create
+                one by clicking the button.
+              </h2>
+            </div>
+          )}
         </div>
         <CreateShowcaseModal setRefetchShowcases={setRefetchShowcases} />
       </div>
-      {!showcases?.length && (
-        <div className="flex gap-[6px]">
-          <p className="text-xl text-default-500">
-            You don&apos;t have any showcases yet. Create one
-          </p>
-          <NavbarLink href="/create" isSelected={false} className="text-xl">
-            here
-          </NavbarLink>
-          <p className="text-xl text-default-500">to get started.</p>
-        </div>
-      )}
       {showcases && showcases.length > 0 && (
         <div className="grid grid-cols-4 gap-6">
           {showcases.map((showcase, i) => (
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import { FRAMES_BASE_PATH } from "@/lib/frames";
 import { fetchMetadata } from "frames.js/next";
 import { Metadata } from "next";
-import { Navbar } from "./components/Navbar";
+import { Navbar, NavbarLink } from "./components/Navbar";
 import { appURL } from "@/lib/utils";
 
 export async function generateMetadata(): Promise<Metadata> {
@@ -16,6 +16,29 @@ export default function Home() {
   return (
     <>
       <Navbar />
+      <div className="flex flex-col items-center gap-12 p-6">
+        <div className="flex flex-col items-center gap-4">
+          <h1 className="text-5xl font-bold">
+            Build in <span className="underline">seconds</span> your{" "}
+            <span className="text-success">Shopify</span> frame
+          </h1>
+          <h2 className="text-3xl text-default-500">
+            Let users fill their 🛒 directly on{" "}
+            <span className="text-primary">Farcaster</span>
+          </h2>
+        </div>
+        <div className="flex gap-2">
+          <p className="text-xl text-default-500">Start now and</p>
+          <NavbarLink
+            href={"/dashboard"}
+            isSelected={false}
+            className="text-xl"
+          >
+            create
+          </NavbarLink>{" "}
+          <p className="text-xl text-default-500">your first Showcase</p>
+        </div>
+      </div>
     </>
   );
 }
