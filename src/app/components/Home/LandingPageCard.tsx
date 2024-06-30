1cf21641304a8ccf79ed4865cf89eea4d5803fb3
caso <matteocasonato@protonmail.com>
2024-06-30 18:44:50 +0200

    Create your first showcase button logs in

--- src/app/components/Home/LandingPageCard.tsx
+++ src/app/components/Home/LandingPageCard.tsx
@@ -2,11 +2,33 @@
 
 import { useState, useRef, useEffect } from "react";
 import * as THREE from "three";
-import { Button, Link } from "@nextui-org/react";
+import { Button, Link, useDisclosure } from "@nextui-org/react";
+import { useLogin, usePrivy } from "@privy-io/react-auth";
+import { customLogin } from "../Navbar";
+import { UpdateShopModal } from "../UpdateShopModal";
+import { useRouter } from "next/navigation";
 
 export const LandingPageCard = () => {
   const [vantaEffect, setVantaEffect] = useState<any>(null);
   const vantaRef = useRef(null);
+  const router = useRouter();
+  const { user } = usePrivy();
+
+  const {
+    isOpen: isOpenUpdate,
+    onOpenChange: onOpenUpdateChange,
+    onClose: onCloseUpdate,
+  } = useDisclosure();
+
+  const { login } = useLogin({
+    onComplete: async (user) => {
+      await customLogin(user, onOpenUpdateChange, router, true);
+    },
+    onError: (error) => {
+      console.error(error);
+      // Any logic you'd like to execute after a user exits the login flow or there is an error
+    },
+  });
 
   useEffect(() => {
     const initializeVanta = async () => {
@@ -41,27 +63,47 @@ export const LandingPageCard = () => {
   }, [vantaEffect]);
 
   return (
-    <div
-      className="flex flex-col items-center gap-12 px-10 py-10 sm:px-20 sm:py-20 w-full m-auto border-2 rounded-3xl border-primary-light bg-[#170021] overflow-hidden shadow-neon"
-      ref={vantaRef}
-    >
-      <div className="flex flex-col items-center text-center gap-4">
-        <h1 className="text-3xl sm:text-5xl font-bold text-balance">
-          Build in <span className="underline">seconds</span> your Shopify frame
-        </h1>
-        <h2 className="text-2xl sm:text-3xl text-default-600 text-balance">
-          Let users fill their 🛒 directly on{" "}
-          <span className="text-primary font-bold">Farcaster</span>
-        </h2>
-      </div>
-      <Button
-        className="bg-primary-light text-black text-xl z-10"
-        size="lg"
-        as={Link}
-        href="/dashboard"
+    <>
+      <UpdateShopModal
+        isOpen={isOpenUpdate}
+        onOpenChange={onOpenUpdateChange}
+        onClose={onCloseUpdate}
+        user={user}
+      />
+      <div
+        className="flex flex-col items-center gap-12 px-10 py-10 sm:px-20 sm:py-20 w-full m-auto border-2 rounded-3xl border-primary-light bg-[#170021] overflow-hidden shadow-neon"
+        ref={vantaRef}
       >
-        Create your first Showcase
-      </Button>
-    </div>
+        <div className="flex flex-col items-center text-center gap-4">
+          <h1 className="text-3xl sm:text-5xl font-bold text-balance">
+            Build in <span className="underline">seconds</span> your Shopify
+            frame
+          </h1>
+          <h2 className="text-2xl sm:text-3xl text-default-600 text-balance">
+            Let users fill their 🛒 directly on{" "}
+            <span className="text-primary font-bold">Farcaster</span>
+          </h2>
+        </div>
+        {!user && (
+          <Button
+            className="bg-primary-light text-black text-xl z-10"
+            size="lg"
+            onClick={login}
+          >
+            Create your first Showcase
+          </Button>
+        )}
+        {user && (
+          <Button
+            className="bg-primary-light text-black text-xl z-10"
+            size="lg"
+            as={Link}
+            href={"/dashboard"}
+          >
+            Create your first Showcase
+          </Button>
+        )}
+      </div>
+    </>
   );
 };
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,26 +1,17 @@
 "use client";
 
 import { Suspense, useContext, useEffect, useState } from "react";
-import { Navbar, customLogin } from "@/app/components/Navbar";
+import { Navbar } from "@/app/components/Navbar";
 import { Showcases } from "@/app/components/Showcases";
 import { Shops } from "@/app/components/Shops";
 import { Shop } from "@/lib/mongodb";
 import { useQuery } from "@tanstack/react-query";
-import { useLogin, usePrivy } from "@privy-io/react-auth";
+import { usePrivy } from "@privy-io/react-auth";
 import { AppContext } from "@/app/providers";
-import { useRouter } from "next/navigation";
-import { useDisclosure } from "@nextui-org/react";
-import { UpdateShopModal } from "@/app/components/UpdateShopModal";
 
 export default function Dashboard() {
   const { user } = usePrivy();
   const context = useContext(AppContext);
-  const router = useRouter();
-  const {
-    isOpen: isOpenUpdate,
-    onOpenChange: onOpenUpdateChange,
-    onClose: onCloseUpdate,
-  } = useDisclosure();
 
   const activeShopId = context?.activeShopId || "";
   const userId = user?.id || "";
@@ -28,16 +19,6 @@ export default function Dashboard() {
   const [refetchShops, setRefetchShops] = useState(false);
   const [shops, setShops] = useState<Shop[] | null>(null);
   const [refetchCount, setRefetchCount] = useState(0);
-
-  const { login } = useLogin({
-    onComplete: async (user) => {
-      await customLogin(user, onOpenUpdateChange, router, true);
-    },
-    onError: (error) => {
-      console.error(error);
-      // Any logic you'd like to execute after a user exits the login flow or there is an error
-    },
-  });
 
   const {
     isLoading: isLoadingShops,
@@ -50,10 +31,6 @@ export default function Dashboard() {
     select: (data) => data.shops,
     enabled: refetchShops,
   });
-
-  useEffect(() => {
-    if (!user) login();
-  }, []);
 
   useEffect(() => {
     if (!!userId) setRefetchShops(true);
@@ -76,12 +53,6 @@ export default function Dashboard() {
       <Suspense>
         <Navbar />
       </Suspense>
-      <UpdateShopModal
-        isOpen={isOpenUpdate}
-        onOpenChange={onOpenUpdateChange}
-        onClose={onCloseUpdate}
-        user={user}
-      />
       <Shops
         userId={userId}
         shops={shops}
